15fd9b20d517c2b21a8783057c705bfe598723b1
Vincent Sandt <vincent@tetraed.re>
2016-04-09 15:47:19 +0200

    design integration

--- src/js/directives.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('mutualab.org',[]);
-
-app.directive('mtSvg',[function(){
-  return {
-      restrict : 'A',
-      scope:{src:"@"},
-      template:'<ng-include src="src"></ng-include>' 
-  };
-}]);
---
+++ src/js/site.js
@@ -0,0 +1,87 @@
+var app = angular.module('mutualab.org',[]);
+
+app.directive('mtSvg',[function(){
+  return {
+      restrict : 'A',
+      scope:{src:"@"},
+      template:'<ng-include src="src"></ng-include>' 
+  };
+}]);
+
+
+app.directive('flickrImg',['FlickrImagesSearch','FlickrImage','utils',
+                   function(FlickrImagesSearch,  FlickrImage,  utils){
+  return {
+    restrict:'A',
+    scope:{search:"@",author:"="},
+    link:function(scope,elt,attr){
+      FlickrImagesSearch.get(attr.search)
+      .then(function(images){
+
+        FlickrImage(utils.randomElt(images).id).then(function(data){
+          elt.css('background-image',"url("+data.url+")");
+          console.log(data)
+          scope.author = data.author;
+        });
+      })
+    }      
+  };
+}]);
+
+app.service('utils',[function(){
+    this.randomElt = function(items){
+      return items[Math.floor(Math.random()*items.length)];
+    }
+}])
+
+
+/**
+ * Flickr get contents from search
+ */
+
+app.constant('flickrApiKey',"367322728086f1d0cffda0170fdcdf7b")
+app.service('FlickrImagesSearch',['$http','$q','flickrApiKey',
+                          function($http,  $q,  flickrApiKey){
+  this.get = function(search){
+    var defrd = $q.defer();
+    var requestUrl = "https://api.flickr.com/services/rest/"+
+                     "?method=flickr.photos.search&api_key="+flickrApiKey+
+                     "&text="+search+"&format=json&nojsoncallback=1"
+
+    $http.get(requestUrl)
+    .then(function(response){
+      defrd.resolve(response.data.photos.photo)
+    });
+    return defrd.promise;
+  };
+}])
+
+
+app.factory('FlickrImage',['$http','$q','flickrApiKey',
+                   function($http,  $q,  flickrApiKey){
+    var defrd = $q.defer();
+  return function(flickrImageId){
+    var requestUrl = "https://api.flickr.com/services/rest/"+
+                 "?method=flickr.photos.getInfo&api_key="+flickrApiKey+
+                 "&photo_id="+flickrImageId+"&format=json&nojsoncallback=1";
+
+    $http.get(requestUrl).then(function(response){
+      var photoResp = response.data.photo;
+      console.log(photoResp)
+      var photo = {
+        url:"https://farm"+photoResp.farm+".staticflickr.com/"+photoResp.server+"/"+photoResp.id+"_"+photoResp.secret+"."+photoResp.originalformat,
+        author:{
+          name:photoResp.owner.username,
+          profile:"https://www.flickr.com/photos/"+photoResp.owner.path_alias || photoResp.owner.username,
+          pic:"https://flickr.com/buddyicons/"+photoResp.owner.nsid+".jpg"
+        },
+        title:photoResp.title._content,
+        description:photoResp.description._content
+      };
+      defrd.resolve(photo)
+    });
+    return defrd.promise;
+  }
+}])
+
+
