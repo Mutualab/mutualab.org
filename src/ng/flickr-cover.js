020f603d0d69f9b44bacf7c967f18d56247be782
Vincent Sandt <vincent@tetraed.re>
2016-08-16 23:15:56 +0200

    get largest image from flickr api, restrict licences

--- src/ng/flickr-cover.js
+++ src/ng/flickr-cover.js
@@ -1,8 +1,14 @@
 var app = angular.module('flickr-cover',['templates']);
 
+/**
+ * Flickr get contents from search
+ */
 
-app.directive('flickrImg',['FlickrImagesSearch','FlickrImage','utils',
-                   function(FlickrImagesSearch,  FlickrImage,  utils){
+app.constant('flickrApiKey',"__FLICKR_API_KEY__")
+
+
+app.directive('flickrImg',['FlickrImagesSearch','FlickrImage','FlkrUtils',
+                   function(FlickrImagesSearch,  FlickrImage,  FlkrUtils){
   return {
     restrict:'A',
     scope:{search:"@",author:"="},
@@ -10,9 +16,9 @@ app.directive('flickrImg',['FlickrImagesSearch','FlickrImage','utils',
       FlickrImagesSearch.get(attr.search)
       .then(function(images){
 
-        FlickrImage(utils.randomElt(images).id).then(function(data){
+        FlickrImage(FlkrUtils.randomElt(images).id).then(function(data){
           elt.css('background-image',"url("+data.url+")");
-          console.log(data)
+          //console.log(data)
           scope.author = data.author;
         });
       })
@@ -20,25 +26,20 @@ app.directive('flickrImg',['FlickrImagesSearch','FlickrImage','utils',
   };
 }]);
 
-app.service('utils',[function(){
+app.service('FlkrUtils',[function(){
     this.randomElt = function(items){
       return items[Math.floor(Math.random()*items.length)];
     }
 }])
 
 
-/**
- * Flickr get contents from search
- */
-
-app.constant('flickrApiKey',"__FLICKR_API_KEY__")
 app.service('FlickrImagesSearch',['$http','$q','flickrApiKey',
                           function($http,  $q,  flickrApiKey){
   this.get = function(search){
     var defrd = $q.defer();
     var requestUrl = "https://api.flickr.com/services/rest/"+
                      "?method=flickr.photos.search&api_key="+flickrApiKey+
-                     "&text="+search+"&format=json&nojsoncallback=1"
+                     "&text="+search+"&format=json&nojsoncallback=1&license=1,2,3,4,5,6,7"
 
     $http.get(requestUrl)
     .then(function(response){
@@ -59,12 +60,19 @@ app.factory('FlickrImage',['$http','$q','flickrApiKey',
 
     $http.get(requestUrl).then(function(response){
       var photoResp = response.data.photo;
+      //console.log(photoResp)
+      //
+      var userSlug;
+      if( !!photoResp.owner.path_alias &&  photoResp.owner.path_alias != 'null') 
+           userSlug = photoResp.owner.path_alias;
+      else userSlug = photoResp.owner.nsid;
+
       console.log(photoResp)
       var photo = {
-        url:"https://farm"+photoResp.farm+".staticflickr.com/"+photoResp.server+"/"+photoResp.id+"_"+photoResp.secret+"."+photoResp.originalformat,
+        url:"https://farm"+photoResp.farm+".staticflickr.com/"+photoResp.server+"/"+photoResp.id+"_"+photoResp.secret+"_b."+photoResp.originalformat,
         author:{
           name:photoResp.owner.username,
-          profile:"https://www.flickr.com/photos/"+photoResp.owner.path_alias || photoResp.owner.username,
+          profile:"https://www.flickr.com/photos/"+userSlug,
           pic:"https://flickr.com/buddyicons/"+photoResp.owner.nsid+".jpg"
         },
         title:photoResp.title._content,
