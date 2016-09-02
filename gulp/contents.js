3e26c915ff024a20f63e47261049d7b204cc6e4f
Vincent Sandt <vincent@tetraed.re>
2016-09-02 14:37:21 +0200

    cms page

--- gulp/contents.js
+++ gulp/contents.js
@@ -3,9 +3,10 @@ const
            glob = require("glob"),
              fs = require('fs'),
       camelCase = require('camel-case'),
-      dashCase = require('camel-2-dash'),
+       dashCase = require('camel-2-dash'),
            path = require('path'),
          marked = require('marked'),
+    frontMatter = require('front-matter'),
             njk = require('nunjucks'),
        gulpData = require('gulp-data'),
    gulpNunjucks = require('gulp-nunjucks'),
@@ -19,6 +20,7 @@ module.exports = (config, bsync) => () => {
     var contentJsonFiles = glob.sync(`${config.contDir}/*.json`),
          componentsFiles = glob.sync(`${config.srcDir}/render/components/*.html`),
           contentMdFiles = glob.sync(`${config.contDir}/*.md`),
+     contentMdPagesFiles = glob.sync(`${config.contDir}/pages/*.md`),
                  cmsData = {"components":{},"contents":{}};
 
 
@@ -45,7 +47,7 @@ module.exports = (config, bsync) => () => {
     function getName(file){
       var ext = path.extname(file);
       var key = path.basename(file, ext);
-      return {cml:camelCase(key),file:key+ext}
+      return {cml:camelCase(key),file:key+ext, filename:key}
     }
 
     contentJsonFiles.forEach((file) => {
@@ -67,6 +69,41 @@ module.exports = (config, bsync) => () => {
         markdownHtml = markdownHtml.replace(/\<p\>\[\%(.*)\%\]<\/p\>/g, '{% include $1 %}');
         cmsData.contents[getName(file).cml] = njkEnv.renderString(markdownHtml, cmsData)
     });
+
+    var defaultPageTemplate = fs.readFileSync(`${config.srcDir}/render/templates/page.html`,'utf-8');
+    contentMdPagesFiles.forEach((file) => {
+        var rawContent = fs.readFileSync(file,'utf-8');
+
+        metadata = frontMatter(rawContent);
+        rawContent = rawContent.replace( /^---([\s\S]*?)---$/gm , '')
+        console.log(rawContent)
+        markdownHtml =  marked(rawContent);
+
+        
+
+        var pageTemplate = null;
+        var fileSlug = getName(file).filename
+        var targetFilename = `${fileSlug}.html`;
+        var templateFile = `${config.srcDir}/render/templates/${targetFilename}`;
+        
+        if (!!fs.existsSync(templateFile)) { 
+          pageTemplate = fs.readFileSync(templateFile,'utf-8'); 
+        } else {
+          pageTemplate = defaultPageTemplate
+        }
+
+        
+         // allow components in markdown contents
+        markdownHtml = markdownHtml.replace(/\<p\>\[\%(.*)\%\]<\/p\>/g, '{% include $1 %}');
+        cmsData.currentPage = {
+            content:markdownHtml,
+            metadata:metadata.attributes,
+        }
+        content = njkEnv.renderString(pageTemplate, cmsData);
+        fs.writeFileSync(`./${config.tmpDir}/${targetFilename}` , content);
+
+    });
+    
     
 
 
