c342ffa2f5e03e8e33039e3ee48233453dd831d1
Vincent Sandt <vincent@tetraed.re>
2016-09-05 14:43:53 +0200

    Remove useless comments

--- src/ng/config.js
+++ src/ng/config.js
@@ -3,5 +3,18 @@ angular.module('mutualab.org')
 
 .constant('Config', {
   googleEventsCalendarId:"rnn0dim2qm94h11qot49smul9g@group.calendar.google.com"
-  
+  "roomCalendars":[
+    {
+      "_id": "57cd67d001bd9e3bbf44c66e",
+       "calendarId":""
+    },
+    {
+      "_id": "57cd67d0f1ccdc12ddf65e77",
+       "calendarId":""
+    },
+    {
+      "_id": "57cd67d08d74c65d5998a690",
+       "calendarId":""
+    },
+  ]
 })
