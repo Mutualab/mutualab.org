ce9ed2a9ecdaaee8294c532d91d208619be853e4
Vincent Sandt <vincent@tetraed.re>
2016-10-04 01:11:04 +0200

    update prose config

--- gulp/images.js
+++ gulp/images.js
@@ -5,6 +5,7 @@ const
 module.exports = (config,bsync) => ()=>{
     return gulp.src(
       [ `./${config.srcDir}/images/**/*.{svg,jpg,png,gif}`,
+        `./${config.contDir}/images/**/*.{svg,jpg,png,gif}`,
         `./images/**/*.{svg,jpg,png,gif}`])
       .pipe( gulp.dest(`./${config.tmpDir}/images`)) 
       .pipe( bsync.stream() );
